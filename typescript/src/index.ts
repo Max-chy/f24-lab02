311191f23f0f33eec81545487cef0857d8b0742e
Max <max@maxdeair.wifi.local.cmu.edu>
2024-09-06 10:33:44 -0400

    update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,6 @@
 import { newRenderer } from "./renderer.js"
 import { Rectangle } from "./shapes/rectangle.js";
 
-const rectangle = new Rectangle(2, 3)
+const rectangle: Shape = new Rectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
